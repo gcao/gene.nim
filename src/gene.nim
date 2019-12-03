2a7b805ce9df575abb92df492208748327bb4b08
Guoliang Cao <gcao99@gmail.com>
2019-12-03 17:32:50 -0500

    Copied EDN parser as a starting point

--- src/gene.nim
+++ src/gene.nim
@@ -1,7 +1,7 @@
 # This is just an example to get you started. A typical hybrid package
 # uses this file as the main entry point of the application.
 
-import gene/parser
+import genepkg/parser
 
 when isMainModule:
   echo "Hello World!"
