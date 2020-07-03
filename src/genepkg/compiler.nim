5bffa558788991e0db6f0e2d1453f177df36348a
Guoliang Cao <gcao99@gmail.com>
2020-07-02 22:31:43 -0400

    WIP: compiler

--- src/genepkg/compiler.nim
+++ src/genepkg/compiler.nim
@@ -14,13 +14,26 @@ type
       value: GeneValue
     else: discard
 
+proc `==`*(this, that: Instruction): bool =
+  if this.is_nil:
+    if that.is_nil: return true
+    return false
+  elif that.is_nil:
+    return false
+  elif this.kind != that.kind:
+    return false
+  else:
+    case this.kind
+    of Init:
+      return true
+    else:
+      return false
+
 proc instr_init*(): Instruction = Instruction(kind: Init)
 
 proc compile*(node: GeneNode): seq[Instruction] =
   result.add(instr_init())
   case node.kind:
-    of GeneNil:
-      discard
     else: discard
 
 proc compile*(nodes: seq[GeneNode]): seq[Instruction] =
