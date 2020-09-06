9251cd231eb7d74f0248a1ba8f7e3f0b9687fb1d
Guoliang Cao <gcao99@gmail.com>
2020-09-06 08:04:17 -0400

    Optimization: use local pos to improve performance

--- src/gene/cpu.nim
+++ src/gene/cpu.nim
@@ -14,22 +14,22 @@ proc run*(self: var VM, module: Module): GeneValue =
   self.cur_block = module.default
 
   var instr: Instruction
-  self.pos = 0
-  while self.pos < self.cur_block.instructions.len:
-    instr = self.cur_block.instructions[self.pos]
-    # debug(&"{self.pos:>4} {instr}")
+  var pos = 0
+  while pos < self.cur_block.instructions.len:
+    instr = self.cur_block.instructions[pos]
+    # debug(&"{pos:>4} {instr}")
     case instr.kind:
     of Default:
-      self.pos += 1
+      pos += 1
       self.cur_stack[0] = instr.val
     of Save:
-      self.pos += 1
+      pos += 1
       self.cur_stack[instr.reg] = instr.val
     of Copy:
-      self.pos += 1
+      pos += 1
       self.cur_stack[instr.reg2] = self.cur_stack[instr.reg]
     of Print, Println:
-      self.pos += 1
+      pos += 1
       let val = self.cur_stack[instr.reg]
       case val.kind:
       of GeneNilKind:
@@ -41,17 +41,17 @@ proc run*(self: var VM, module: Module): GeneValue =
       if instr.kind == Println:
         stdout.write("\n")
     of Global:
-      self.pos += 1
+      pos += 1
       self.cur_stack[0] = new_gene_internal(APP.ns)
     of Self:
-      self.pos += 1
+      pos += 1
       self.cur_stack[0] = self.cur_stack.self
     of DefMember:
-      self.pos += 1
+      pos += 1
       let key = instr.reg
       self.cur_stack.cur_scope[key] = self.cur_stack[0]
     of DefNsMember:
-      self.pos += 1
+      pos += 1
       let name = instr.val
       case name.kind:
       of GeneSymbol:
@@ -75,11 +75,11 @@ proc run*(self: var VM, module: Module): GeneValue =
       else:
         not_allowed()
     of GetMember:
-      self.pos += 1
+      pos += 1
       var key = instr.reg
       self.cur_stack[0] = self.cur_stack.get(key)
     of GetNestedNsMember:
-      self.pos += 1
+      pos += 1
       var name = instr.val.csymbol
       var ns: Namespace
       case name.first:
@@ -95,54 +95,54 @@ proc run*(self: var VM, module: Module): GeneValue =
       var key = name.rest[^1].hash
       self.cur_stack[0] = ns[key]
     of Add:
-      self.pos += 1
+      pos += 1
       let first = self.cur_stack[instr.reg].num
       let second = self.cur_stack[instr.reg2].num
       self.cur_stack[0] = new_gene_int(first + second)
     of AddI:
-      self.pos += 1
+      pos += 1
       let first = self.cur_stack[instr.reg].num
       let second = instr.val.num
       self.cur_stack[0] = new_gene_int(first + second)
     of Sub:
-      self.pos += 1
+      pos += 1
       let first = self.cur_stack[instr.reg].num
       let second = self.cur_stack[instr.reg2].num
       self.cur_stack[0] = new_gene_int(first - second)
     of SubI:
-      self.pos += 1
+      pos += 1
       let first = self.cur_stack[instr.reg].num
       let second = instr.val.num
       self.cur_stack[0] = new_gene_int(first - second)
     of Lt:
-      self.pos += 1
+      pos += 1
       let first = self.cur_stack[instr.reg].num
       let second = self.cur_stack[instr.reg2].num
       self.cur_stack[0] = new_gene_bool(first < second)
     of LtI:
-      self.pos += 1
+      pos += 1
       let first = self.cur_stack[instr.reg].num
       let second = instr.val.num
       self.cur_stack[0] = new_gene_bool(first < second)
     of Jump:
-      self.pos = cast[int](instr.val.num)
+      pos = cast[int](instr.val.num)
     of JumpIfFalse:
       if self.cur_stack[instr.reg].isTruthy:
-        self.pos += 1
+        pos += 1
       else:
-        self.pos = cast[int](instr.val.num)
+        pos = cast[int](instr.val.num)
     of CreateFunction:
-      self.pos += 1
+      pos += 1
       var fn = instr.val
       let key = cast[Hash](fn.internal.fn.name.hash)
       self.cur_stack.cur_ns[key] = fn
       self.cur_stack[0] = instr.val
     of CreateArguments:
-      self.pos += 1
+      pos += 1
       var args = instr.val
       self.cur_stack[instr.reg] = args
     of CreateNamespace:
-      self.pos += 1
+      pos += 1
       var name = instr.val.str
       var ns = new_namespace(name)
       var val = new_gene_internal(ns)
@@ -150,7 +150,7 @@ proc run*(self: var VM, module: Module): GeneValue =
       self.cur_stack.cur_ns[key] = val
       self.cur_stack[0] = val
     of Import:
-      self.pos += 1
+      pos += 1
       var module = self.cur_stack[0].str
       var ns: Namespace
       if not APP.namespaces.hasKey(module):
@@ -164,7 +164,7 @@ proc run*(self: var VM, module: Module): GeneValue =
         let key = cast[Hash](s.hash)
         self.cur_stack.cur_ns[key] = ns[key]
     of ImportNative:
-      self.pos += 1
+      pos += 1
       var module = self.cur_stack[0].str
       var names = instr.val.vec.map(proc(v: GeneValue): string = v.symbol)
       var mappings = load_dynamic(module, names)
@@ -172,7 +172,7 @@ proc run*(self: var VM, module: Module): GeneValue =
         let key = cast[Hash](name.hash)
         self.cur_stack.cur_ns[key] = new_gene_internal(mappings[name])
     of CreateClass:
-      self.pos += 1
+      pos += 1
       var name = instr.val.str
       var class = new_class(name)
       var val = new_gene_internal(class)
@@ -180,12 +180,12 @@ proc run*(self: var VM, module: Module): GeneValue =
       self.cur_stack.cur_ns[key] = val
       self.cur_stack[0] = val
     of CreateMethod:
-      self.pos += 1
+      pos += 1
       var fn = self.cur_stack[0].internal.fn
       var class = self.cur_stack.self.internal.class
       class.methods[fn.name] = fn
     of CreateInstance:
-      self.pos += 1
+      pos += 1
       var class = self.cur_stack[0].internal.class
       var instance = new_gene_instance(new_instance(class))
       self.cur_stack[0] = instance
@@ -199,28 +199,28 @@ proc run*(self: var VM, module: Module): GeneValue =
         self.cur_stack.self = instance
         self.cur_stack.caller_stack = stack
         self.cur_stack.caller_blk = self.cur_block
-        self.cur_stack.caller_pos = self.pos
+        self.cur_stack.caller_pos = pos
         for i in 0..<fn.args.len:
           var arg = fn.args[i]
           var val = args[i]
           let key = cast[Hash](arg.hash)
           self.cur_stack.cur_scope[key] = val
         self.cur_block = fn.body_block
-        self.pos = 0
+        pos = 0
     of PropGet:
-      self.pos += 1
+      pos += 1
       var name = instr.val.str
       var this = self.cur_stack[0]
       var val = this.instance.value.gene_props[name]
       self.cur_stack[0] = val
     of PropSet:
-      self.pos += 1
+      pos += 1
       var name = instr.val.str
       var val = self.cur_stack[instr.reg]
       self.cur_stack.self.instance.value.gene_props[name] = val
 
     of InvokeMethod:
-      self.pos += 1
+      pos += 1
       var this = self.cur_stack[instr.reg]
       var name = instr.val.str
       var fn = this.instance.class.methods[name]
@@ -237,13 +237,13 @@ proc run*(self: var VM, module: Module): GeneValue =
         cur_stack.cur_scope[key] = val
       cur_stack.caller_stack = stack
       cur_stack.caller_blk = self.cur_block
-      cur_stack.caller_pos = self.pos
+      cur_stack.caller_pos = pos
       self.cur_stack = cur_stack
       self.cur_block = fn.body_block
-      self.pos = 0
+      pos = 0
 
     of Call:
-      self.pos += 1
+      pos += 1
       var stack = self.cur_stack
       var fn = stack[0].internal.fn
       var args = stack[instr.reg].internal.args
@@ -257,13 +257,13 @@ proc run*(self: var VM, module: Module): GeneValue =
         cur_stack.cur_scope[key] = val
       cur_stack.caller_stack = stack
       cur_stack.caller_blk = self.cur_block
-      cur_stack.caller_pos = self.pos
+      cur_stack.caller_pos = pos
       self.cur_stack = cur_stack
       self.cur_block = fn.body_block
-      self.pos = 0
+      pos = 0
 
     of CallNative:
-      self.pos += 1
+      pos += 1
       var name = instr.val.str
       case name:
       of "str_len":
@@ -274,13 +274,13 @@ proc run*(self: var VM, module: Module): GeneValue =
         todo(name)
 
     of InvokeNative:
-      self.pos += 1
+      pos += 1
       var target = self.cur_stack[instr.reg].internal.native_proc
       var args = self.cur_stack[instr.reg2].internal.args
       self.cur_stack[0] = target(args.positional)
 
     of CallBlock:
-      self.pos += 1
+      pos += 1
       var stack = self.cur_stack
       var cur_stack = StackMgr.get
       cur_stack.cur_ns = stack.cur_ns
@@ -288,10 +288,10 @@ proc run*(self: var VM, module: Module): GeneValue =
       cur_stack.self = stack[instr.reg2]
       cur_stack.caller_stack = stack
       cur_stack.caller_blk = self.cur_block
-      cur_stack.caller_pos = self.pos
+      cur_stack.caller_pos = pos
       self.cur_stack = cur_stack
       self.cur_block = stack[instr.reg].internal.blk
-      self.pos = 0
+      pos = 0
 
     of CallEnd:
       var stack = self.cur_stack
@@ -299,12 +299,12 @@ proc run*(self: var VM, module: Module): GeneValue =
       if not self.cur_block.no_return:
         self.cur_stack[0] = stack[0]
       self.cur_block = stack.caller_blk
-      self.pos = stack.caller_pos
+      pos = stack.caller_pos
       ScopeMgr.free(stack.cur_scope)
       StackMgr.free(stack)
 
     of SetItem:
-      self.pos += 1
+      pos += 1
       var val = self.cur_stack[instr.reg]
       var index = instr.val.num
       if val.kind == GeneInternal and val.internal.kind == GeneArguments:
@@ -312,7 +312,7 @@ proc run*(self: var VM, module: Module): GeneValue =
       else:
         todo($instr)
     else:
-      # self.pos += 1
+      # pos += 1
       todo($instr)
 
   result = self.cur_stack.default
