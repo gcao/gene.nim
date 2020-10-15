2922d4b14b9f4fa9f8cde6876bb28834a49547ae
Guoliang Cao <gcao99@gmail.com>
2020-10-14 21:32:29 -0400

    Pattern matching

---
+++ src/gene/pattern_matching.nim
@@ -0,0 +1,38 @@
+import ./types
+
+type
+  # # Pattern matching
+  # MatcherKind* = enum
+  #   MatchRoot
+  #   MatchPlaceholder # _
+  #   MatchOne
+  #   MatchMany
+  #   MatchExact # exact symbol as marker
+
+  # Matcher* = ref object
+  #   name*: string
+  #   required*: bool
+  #   case kind*: MatcherKind
+  #   of MatchOne:
+  #     one_default*: GeneValue # Expression for default value
+  #   of MatchExact:
+  #     exact*: string
+  #   else:
+  #     discard
+
+  Matcher* = ref object of RootObj
+    name*: string
+    required*: bool
+
+  MatcherGroup* = ref object of Matcher
+    children*: Matcher
+
+  PlaceholderMatcher* = ref object of Matcher
+
+  RootMatcher* = ref object of Matcher
+
+##################################################
+
+# proc new_matchers(v: GeneValue): Matchers =
+#   case v.kind:
+#   of GeneSymbol
--- tests/test_pattern_matching.nim
+++ tests/test_pattern_matching.nim
@@ -5,11 +5,42 @@ import ./helpers
 # Pattern Matching 
 #
 # * Argument parsing
-# * (match (…) value)
+# * (match pattern input)
 #   Match works similar to argument parsing
 # * Custom matchers can be created, which takes something and
-#   returns a function that takes a value and a scope object and
-#   parses the value and stores as one or multiple variables
+#   returns a function that takes an input and a scope object and
+#   parses the input and stores as one or multiple variables
+
+# Mode: argument, match, ...
+# When matching arguments, root level name will match first item in the input
+# While (match name) will match the whole input
+#
+# Root level
+# (match name input)
+# (match _ input)
+#
+# Child level
+# (match [a? b] input) # "a" is optional, if input contains only one item, it'll be
+#                      # assigned to "b"
+# (match [a... b] input) # "a" will match 0 to many items, the last item is assigned to "b"
+# (match [a = 1 b] input) # "a" is optional and has default value of 1
+#
+# Grandchild level
+# (match [a b [c]] input) # "c" will match a grandchild
+#
+# Match properties
+# (match [^a] input)  # "a" will match input's property "a"
+# (match [^a!] input) # "a" will match input's property "a" and is required
+# (match [^a: var_a] input) # "var_a" will match input's property "a"
+# (match [^a: var_a = 1] input) # "var_a" will match input's property "a", and has default
+#                               # value of 1
+#
+# Q: How do we match gene_op?
+# A: Use "*" to signify it. like "^" to signify properties. It does not support optional,
+#    default values etc
+#    [*op] will assign gene_op to "op"
+#    [*: [...]] "*:" or "*name:" will signify that next item matches gene_op's internal structure
+#
 
 test_interpreter """
   (fn f a
@@ -41,3 +72,17 @@ test_interpreter """
   (match [a b] x)
   b
 """, GeneNil
+
+# test_interpreter """
+#   (match
+#     [:if cond :then logic1... :else logic2...]
+#     :[if true then
+#       (do A)
+#       (do B)
+#     else
+#       (do C)
+#       (do D)
+#     ]
+#   )
+#   cond
+# """, true
