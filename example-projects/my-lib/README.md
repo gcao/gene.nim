0e75ffc3097a00b56581075818112b4cbfc2aa62
Guoliang Cao <gcao99@gmail.com>
2020-11-12 08:57:07 -0500

    stdlib: tests

--- example-projects/my-lib/README.md
+++ example-projects/my-lib/README.md
@@ -1,1 +1,10 @@
 # My-lib - An example library project
+
+## Tasks
+
+- [ ] TDD - tests and code written in .gene
+- [ ] Call dependencies
+- [ ] Native extension
+- [ ] Wrap up native extension in .gene
+- [ ] Test native extension in .gene
+- [ ] Call native extension from dependencies (thru API exposed in Gene)
--- tests/test_stdlib_tests.nim
+++ tests/test_stdlib_tests.nim
@@ -2,18 +2,24 @@ import ./helpers
 
 test_core """
 
-(import genex/tests/[test skip_test])
+(import genex/tests/[suite test skip_test])
 
-(test "A basic test"
-  (assert true)
-)
+(suite "A suite"
+  (test "A basic test"
+    (assert true)
+  )
 
-(skip_test "A failing test"
-  (assert false)
-)
+  (test "A failing test"
+    (assert false)
+  )
 
-(skip_test "Another failing test"
-  (fail)
+  (skip_test "A failing test"
+    (assert false)
+  )
+
+  (skip_test "Another failing test"
+    (fail)
+  )
 )
 
 """
