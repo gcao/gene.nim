36d6ad79b25adfd1fd3db9d485f7a50b57676273
Guoliang Cao <gcao99@gmail.com>
2020-12-06 19:47:59 -0500

    Optimization: minimize allocation

--- src/gene/map_key.nim
+++ src/gene/map_key.nim
@@ -23,8 +23,20 @@ converter to_key*(s: string): MapKey {.inline.} =
 converter key_to_s*(self: MapKey): string {.inline.} =
   result = Keys[cast[int](self)]
 
+converter to_strings*(self: seq[MapKey]): seq[string] {.inline.} =
+  for k in self:
+    result.add(k)
+
+converter to_keys*(self: seq[string]): seq[MapKey] {.inline.} =
+  for item in self:
+    result.add(item)
+
 proc `==`*(this, that: MapKey): bool {.inline.} =
   result = cast[int](this) == cast[int](that)
 
 proc hash*(self: MapKey): Hash {.inline.} =
   result = cast[int](self)
+
+# Add "" as the first
+let EMPTY_STRING_KEY* = add_key("")
+let ROOT_NS_KEY* = add_key("<root>")
