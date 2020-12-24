a39968f3e50ae5f188415381154ea35c3a194682
Guoliang Cao <gcao99@gmail.com>
2020-12-24 10:00:41 -0500

    Document some ideas: tco, hot reloading, case...when...else etc

--- tests/test_interpreter.nim
+++ tests/test_interpreter.nim
@@ -360,3 +360,27 @@ test_interpreter """
   $app
 """, proc(r: GeneValue) =
   check r.internal.app.ns.name == "global"
+
+# case...when...else statement
+# `when` accepts integers, strings, chars, symbols, enum values, and functions
+# (case input
+# when 1
+#   ...
+# when [2 3]
+#   ...
+# else
+#   ...
+# )
+
+# HOT RELOAD
+# Module must be marked as reloadable first
+# We need symbol table per module
+# Symbols are referenced by names/keys
+# Should work for imported symbols, e.g. (import a from "a")
+# Should work for aliases when a symbol is imported, e.g. (import a as b from "a")
+# Should not reload `b` if `b` is defined like (import a from "a") (var b a)
+# Should work for child members of imported symbols, e.g. (import a from "a") a/b
+
+# Reload occurs in the same thread at controllable interval.
+# https://github.com/paul-nameless/nim-fswatch
+# https://github.com/FedericoCeratto/nim-fswatch
